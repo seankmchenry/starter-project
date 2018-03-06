670367252b941a2a884845fdd06a762b348be794
Sean McHenry <seankmchenry@gmail.com>
2018-03-06 12:37:24 -0500

    Updated search form

--- content/themes/starter-theme/searchform.php
+++ content/themes/starter-theme/searchform.php
@@ -8,7 +8,7 @@
 
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
   <!-- Label -->
-  <label><span class="screen-reader-text hide"><?php echo _x( 'Search for:', 'label' ) ?></span></label>
+  <label class="search-label hide"><?php echo _x( 'Search for:', 'label' ) ?></label>
   <!-- Search -->
   <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
   <!-- Submit -->
